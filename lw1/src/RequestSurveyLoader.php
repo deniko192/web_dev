b0b4f2601a363909f0739789c50e1a2a850f1636
deniko192 <59892020+deniko192@users.noreply.github.com>
2022-04-03 15:21:30 +0300

    Update RequestSurveyLoader.php

--- lw1/src/RequestSurveyLoader.php
+++ lw1/src/RequestSurveyLoader.php
@@ -7,9 +7,7 @@ class RequestSurveyLoader
     {
         foreach ($params as $param)
         {
-            $this->surveyData[$param] = isset($_GET[$param]) && !empty($_GET[$param])
-                ? $_GET[$param]
-                : null;
+            $this->surveyData[$param] = $this->getGetParameter($param); 
         }
     }
 
@@ -17,4 +15,11 @@ class RequestSurveyLoader
     {
         return new Survey($this->surveyData['email'], $this->surveyData['first_name'], $this->surveyData['last_name'], $this->surveyData['age']);
     }
+    
+    private function getGetParameter(string $param): ?string
+    {
+        return isset($_GET[$param]) && !empty($_GET[$param])
+            ? $_GET[$param]
+            : null;
+    }   
 }
